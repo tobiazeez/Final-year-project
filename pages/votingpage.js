c515f53dd6a174c33688fe53056952524ea6ce66
Tobi Azeez <azeez.oluwatobi@stu.cu.edu.ng>
2023-05-15 15:30:35 +0100

    bearer

--- pages/votingpage.js
+++ pages/votingpage.js
@@ -1,37 +1,58 @@
 import Image from "next/image";
 import aeies from "/public/images/aeies.png";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { MdDashboard } from "react-icons/md";
 import { BsPeople } from "react-icons/bs";
 import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
+import axios from "axios";
 
 export default function VotingPage() {
   const [isCollapsedSidebar, toggleSidebarCollapse] = useState(false);
-  const allRoles = Object.keys(listOfCandidates);
-  const [currentRole, setCurrentRole] = useState(allRoles[0]);
-  const candidatesToShow = listOfCandidates[currentRole];
+  const [listOfCandidates, setListOfCandidates] = useState({});
+  const [currentRole, setCurrentRole] = useState("");
   const [selectedCandidate, setSelectedCandidate] = useState(null);
+
+  useEffect(() => {
+    // Fetch the candidates from the backend/server
+    getCandidates();
+  }, []);
+
+  const getCandidates = async () => {
+    try {
+      const response = await axios.get(
+        "https://voting-app.adaptable.app/api/v1/candidate/position?position=president"
+      );
+      console.log("response", response.data); // Log the response data
+      if (response.data.success) {
+        setListOfCandidates(response.data.data);
+        setCurrentRole(Object.keys(response.data.data)[0]);
+      }
+      console.log(response.data.data); // Log the list of candidates
+      console.log(response.data.data[0].name); // Log the name of the first candidate
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const toggleSidebarCollapseHandler = () => {
     toggleSidebarCollapse((prev) => !prev);
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (selectedCandidate !== null) {
+      const allRoles = Object.keys(listOfCandidates);
       const indexOfCurrentRole = allRoles.findIndex(
         (role) => role === currentRole
       );
-      console.log(indexOfCurrentRole, "Index of current role");
-
       const indexOfNextRole = indexOfCurrentRole + 1;
-      const nextRole = allRoles[indexOfNextRole];
 
       if (indexOfNextRole < allRoles.length) {
         setCurrentRole(allRoles[indexOfNextRole]);
         setSelectedCandidate(null);
         alert(
-          `You have voted for ${candidatesToShow[selectedCandidate].name}. Thank you for voting!`
+          `You have voted for ${listOfCandidates[currentRole][selectedCandidate].name}. Thank you for voting!`
         );
       } else {
         alert("Success! VOTES SUBMITTED SUCCESSFULLY");
@@ -112,7 +133,7 @@ export default function VotingPage() {
           </h6>
           <form onSubmit={handleSubmit}>
             <div className="body">
-              {candidatesToShow.map((candidate, i) => {
+              {listOfCandidates[currentRole]?.map((candidate, i) => {
                 const { name, course, level, manifesto, image_url } = candidate;
                 return (
                   <div
@@ -166,184 +187,3 @@ export default function VotingPage() {
     </div>
   );
 }
-
-const listOfCandidates = {
-  president: [
-    {
-      name: "Azeez Oluwatobi",
-      course: "Computer Engineering",
-      level: 500,
-      manifesto: "I do not like to brag, just vote for me and see.",
-      image_url: "/images/Tobsss.jpeg",
-    },
-    {
-      name: "Jane Doe",
-      course: "Information and Communication Engineering",
-      level: 400,
-      manifesto: "Emi lokan",
-      image_url: "/images/IY.jpeg",
-    },
-    {
-      name: "John Doe",
-      course: "Electrical and Electronics Engineering",
-      level: 300,
-      manifesto: "I will show you a town hall different from balabalu.",
-      image_url: "/images/Vicky.jpeg",
-    },
-  ],
-  vice_president: [
-    {
-      name: "Olanipekun Temiloluwa",
-      course: "Electrical and Information Engineering",
-      level: 500,
-      manifesto: "Emi Lokan.",
-      image_url: "/images/aeies.png",
-    },
-    {
-      name: "Onojake Mercy",
-      course: "Information and Communication Engineering",
-      level: 400,
-      manifesto: "Don't you like good things.",
-      image_url: "/images/aeies.png",
-    },
-    {
-      name: "Jewel David",
-      course: "Computer Engineering",
-      level: 300,
-      manifesto: "Jesus is Lord.",
-      image_url: "/images/aeies.png",
-    },
-  ],
-  executive_secretary: [
-    {
-      name: "Victoria",
-      course: "Computer Engineering",
-      level: 100,
-      manifesto: "I'm shy,lol",
-      image_url: "/images/aeies.png",
-    },
-    {
-      name: "Inyene",
-      course: "ICE",
-      level: 200,
-      manifesto: "Please vote for me",
-      image_url: "/images/aeies.png",
-    },
-  ],
-  financial_secretary: [
-    {
-      name: "Jane Madu",
-      course: "Electrical and Information Engineering",
-      level: 500,
-      manifesto: "I do not like to brag, just vote for me and see.",
-      image_url: "/images/aeies.png",
-    },
-    {
-      name: "Sharon Chinedu",
-      course: "Information and Communication Engineering",
-      level: 500,
-      manifesto:
-        "I just dey contest, make e no look like say I no serious with my life.",
-      image_url: "/images/aeies.png",
-    },
-    {
-      name: "Jim Rod",
-      course: "Electrical and Electronics Engineering",
-      level: 300,
-      manifesto: "I will show you a town hall different from balabalu.",
-      image_url: "/images/aeies.png",
-    },
-  ],
-  welfare_officer: [
-    {
-      name: "Blessing James",
-      course: "Electrical and Information Engineering",
-      level: 500,
-      manifesto: "Your comfort is my care.",
-      image_url: "/images/aeies.png",
-    },
-    {
-      name: "Job Dan",
-      course: "Information and Communication Engineering",
-      level: 400,
-      manifesto: "I will treat you well.",
-      image_url: "/images/aeies.png",
-    },
-    {
-      name: "Hill Crest",
-      course: "Electrical and Electronics Engineering",
-      level: 300,
-      manifesto: "I am the one for you.",
-      image_url: "/images/aeies.png",
-    },
-    {
-      name: "Sommy Vick",
-      course: "Electrical and Electronics Engineering",
-      level: 500,
-      manifesto: "Anything for you.",
-      image_url: "/images/aeies.png",
-    },
-  ],
-  public_relations_officer: [
-    {
-      name: "Osahon Orha",
-      course: "Electrical and Information Engineering",
-      level: 500,
-      manifesto: "Make we relate.",
-      image_url: "/images/aeies.png",
-    },
-    {
-      name: "Okey Bakassi",
-      course: "Electrical and Information Engineering",
-      level: 400,
-      manifesto: "I will treat you well.",
-      image_url: "/images/aeies.png",
-    },
-    {
-      name: "Basket Mouth",
-      course: "Electrical and Electronics Engineering",
-      level: 300,
-      manifesto: "I will make you laugh.",
-      image_url: "/images/aeies.png",
-    },
-  ],
-  events_officer: [
-    {
-      name: "Davido",
-      course: "Electrical and Information Engineering",
-      level: 500,
-      manifesto: "When I look into your eyes, all i see is your waist",
-      image_url: "/images/aeies.png",
-    },
-    {
-      name: "Burna Boy",
-      course: "Information and Communication Engineering",
-      level: 400,
-      manifesto: "I got a lot of enemies, I'm a baboon,I libe in the zoo.",
-      image_url: "/images/aeies.png",
-    },
-    {
-      name: "Wiz Kid",
-      course: "Electrical and Electronics Engineering",
-      level: 300,
-      manifesto: "I tell you say...",
-      image_url: "/images/aeies.png",
-    },
-  ],
-  academic_officer: [
-    {
-      name: "Mayorkun Baby",
-      course: "Electrical and Electronics Engineering",
-      level: 500,
-      manifesto: "Let us help each other graduate.",
-      image_url: "/images/aeies.png",
-    },
-    {
-      name: "Elijah Nelson",
-      course: "Information and Communication Engineering",
-      level: 400,
-      manifesto: "I will make academics fun.",
-      image_url: "/images/aeies.png",
-    },
-  ],
-};
